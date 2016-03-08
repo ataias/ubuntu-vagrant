383b75f7693749f1b09658c20b44eea486f5e724
Ataias Pereira Reis <ataiasreis@gmail.com>
2016-03-08 13:57:13 -0300

    Added Brazil as mirror and installing latex

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,9 @@
+#Change default repos
+
+#Comment the next two lines if you are not in Brazil
+cp /etc/apt/sources.list /etc/apt/sources.list~
+sed -i 's/http:\/\/archive.ubuntu.com/http:\/\/ubuntu.c3sl.ufpr.br/g' /etc/apt/sources.list
+
 #New repositories
 add-apt-repository ppa:staticfloat/juliareleases
 
@@ -13,7 +19,11 @@ apt-get install -y zsh
 #Install Python
 apt-get install -y python3 python3-pip
 apt-get install -y python3-matplotlib python3-numpy
+apt-get install -y python3-h5py
 pip3 install dropbox
+
+#Latex
+apt-get install -y texlive-latex-base texlive-latex-extra texlive-fonts-recommended
 
 #Change default shell of user vagrant
 chsh -s `which zsh` vagrant
